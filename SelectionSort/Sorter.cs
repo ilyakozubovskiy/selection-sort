177161e7cc501408679df180034968878bb8a513
ilya kozubovskiy <ilyakozubovskiy@outlook.com>
2021-12-28 11:26:54 +0300

    Selection Sort Algorithm

--- SelectionSort/Sorter.cs
+++ SelectionSort/Sorter.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 // ReSharper disable InconsistentNaming
 #pragma warning disable SA1611
@@ -12,8 +12,26 @@ namespace SelectionSort
         /// </summary>
         public static void SelectionSort(this int[] array)
         {
-            // TODO #1. Implement the method using a loop statements.
-            throw new NotImplementedException();
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            for (int startIndex = 0; startIndex < array.Length - 1; startIndex++)
+            {
+                int minIndex = startIndex;
+                for (int currentIndex = startIndex + 1; currentIndex < array.Length; currentIndex++)
+                {
+                    if (array[currentIndex] < array[minIndex])
+                    {
+                        minIndex = currentIndex;
+                    }
+                }
+
+                int tmp = array[minIndex];
+                array[minIndex] = array[startIndex];
+                array[startIndex] = tmp;
+            }
         }
 
         /// <summary>
@@ -21,8 +39,35 @@ namespace SelectionSort
         /// </summary>
         public static void RecursiveSelectionSort(this int[] array)
         {
-            // TODO #2. Implement the method using recursion algorithm.
-            throw new NotImplementedException();
+            RecursiveSelectionSort(array, 0);
+        }
+
+        public static void RecursiveSelectionSort(int[] array, int startIndex)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (array.Length == 0 || startIndex == array.Length - 1)
+            {
+                return;
+            }
+
+            int minIndex = startIndex;
+            for (int currentIndex = startIndex + 1; currentIndex < array.Length; currentIndex++)
+            {
+                if (array[currentIndex] < array[minIndex])
+                {
+                    minIndex = currentIndex;
+                }
+            }
+
+            int tmp = array[minIndex];
+            array[minIndex] = array[startIndex];
+            array[startIndex] = tmp;
+
+            RecursiveSelectionSort(array, startIndex + 1);
         }
     }
 }
